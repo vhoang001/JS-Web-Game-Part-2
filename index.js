a769a487aca7f951a54180952a4fadf297283269
Van Hoang <hoangduykhanhvan@gmail.com>
2024-01-18 20:43:33 -0800

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function newImage(url, left, bottom){
+    // Define Create a new image function
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
@@ -9,11 +10,21 @@ function newImage(url, left, bottom){
 }
 
 function newItem(url, left, bottom){
+    //Define create new item function
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
+//Create the global inventory variable
+let inventory
 function newInventory(){
-    let inventory = document.createElement('div')
+    // Function to add the element to the inventory
+    inventory = document.createElement('div')  //assign the inventory variable 
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -26,9 +37,17 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
+
+//Run the code by calling the function defined aboved
+
+//Run the function newInventory()
 newInventory()
+
+
+//Run the function: newImage()
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
@@ -36,6 +55,9 @@ newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
 
+//Run the function: newItem()
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+
+
